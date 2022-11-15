eff97a13f6b1427ca3eb0012f7c393d87a6a9b49
Rysth <johnpalacios.@gmail.com>
2022-11-15 10:44:48 -0500

    JS Implemented

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const toggler = document.querySelector("#toggler");
+const nav = document.querySelector("nav");
+const list = document.querySelector("#navbar-list");
+const items = document.querySelectorAll("#navbar-list li a");
+
+const mediaQuery = window.matchMedia("(min-width: 1000px)");
+
+toggler.addEventListener("click", () => {
+  list.classList.toggle("flex-column");
+});
+
+items.forEach(element => {
+  element.addEventListener("click", () => {
+    list.classList.toggle("flex-column");
+  });
+});
